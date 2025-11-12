26437abd72c6f9dabd0e47daeaa84cafc8bd4353
Edervalg <edervalnetogomes@hotmail.com>
2025-11-12 16:57:37 -0300

    iniciando projeto

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import * as eva from '@eva-design/eva';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ApplicationProvider } from '@ui-kitten/components';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import React from 'react';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
+const queryClient = new QueryClient();
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <QueryClientProvider client={queryClient}>
+        <Stack>
+        </Stack>
+      </QueryClientProvider>
+    </ApplicationProvider>
   );
 }
